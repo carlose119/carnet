49159d281e5dd0b9db93d95b60c060b31e2839d6
Carlos Carrillo <carlos.e119@gmail.com>
2025-03-27 22:35:47 -0400

    reverso carnet estudiante

--- resources/views/pdf/carnet.blade.php
+++ resources/views/pdf/carnet.blade.php
@@ -1,4 +1,6 @@
 @php
+    use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
     $ruta_public = str_replace(DIRECTORY_SEPARATOR . 'app', '', app_path('public'));
     $ruta_base = str_replace(DIRECTORY_SEPARATOR . 'app', '', app_path('storage'));
     $ruta_imagen = $ruta_base . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, $estudiante->carreras->areas->imagen_carnet);
@@ -13,6 +15,14 @@
     $ruta_imagen_estudiante = $ruta_base . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, $estudiante->imagen);
 
     $ruta_imagen_autoridad_sello = $ruta_base. DIRECTORY_SEPARATOR. 'app'. DIRECTORY_SEPARATOR. 'public'. DIRECTORY_SEPARATOR. str_replace('/', DIRECTORY_SEPARATOR, $autoridad->sello);
+
+    $datos_estudiante = json_encode([
+        'nombre' => strtolower($estudiante->nombre),
+        'cedula' => strtolower($estudiante->cedula),
+        'carrera' => strtolower($estudiante->carreras->nombre),
+    ]);
+
+    $qr_code = base64_encode(QrCode::format('png')->size(100)->generate($datos_estudiante));
 @endphp
 
 <!--Frontal-->
@@ -44,10 +54,23 @@
 </div>
 
 <div style="border: 0px solid #000; position: absolute; width: 204.10px; margin-left: 300px; margin-top: 200px; text-align: left; display: flex; justify-content: center; padding-left: 10px;">
-    <img src="{{ $ruta_imagen_autoridad_sello }}" style="width:90px;">
+    <img src="data:image/png;base64,{{ $qr_code }}" style="width:90px;">
     <img src="{{ $ruta_imagen_autoridad_sello }}" style="width:90px;">
 </div>
 
-{{-- <div style="border: 1px solid #000; position: absolute; width: 204.10px; margin-left: 310px; margin-top: 200px; text-align: right;">
-    <img src="{{ $ruta_imagen_autoridad_sello }}" style="width:90px;">
-</div> --}}
+<div style="border: 0px solid #000; position: absolute; width: 204.10px; margin-left: 300px; margin-top: 270px; text-align: left; display: flex; justify-content: center; padding-left: 10px;">
+    <table>
+        <tr>
+            <td style="font-family: Arial; font-size: 8px; width:90px;">
+                Vence {{ date('d-m-Y', strtotime($carnet->fecha_vencimiento)) }}
+            </td>
+            <td style="font-family: Arial; font-size: 8px; width:90px; text-align: center;">
+                {{ $autoridad->nombre }}<br/>
+                {{ $autoridad->cargo }}<br/>
+                <span style="font-size: 6px;">
+                    {{ $autoridad->resolucion }}
+                </span>                
+            </td>
+        </tr>
+    </table>
+</div>
