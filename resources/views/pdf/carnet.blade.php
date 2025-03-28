b1d1c68c4e115adcfcec4d84abe28acfd33a5c02
Carlos Carrillo <carlos.e119@gmail.com>
2025-03-27 23:26:38 -0400

    ajuste carnet estudiante

--- resources/views/pdf/carnet.blade.php
+++ resources/views/pdf/carnet.blade.php
@@ -27,7 +27,7 @@
 
 <!--Frontal-->
 
-<div style="position: absolute; width: 204.10px; height: 323.15px; margin-left: 0px; margin-top: 0px;">
+{{-- <div style="position: absolute; width: 204.10px; height: 323.15px; margin-left: 0px; margin-top: 0px;">
     <img src="{{ $ruta_imagen }}" style="width: 204.10px; height: 323.15px;">
 </div>
 
@@ -45,11 +45,11 @@
 
 <div style="position: absolute; font-family: Arial; margin-top: 250px; font-size: 10px; text-align: center; font-weight: bold; width: 170px; padding-left: 10px; padding-right: 10px;">
     {{ strtoupper($estudiante->carreras->nombre) }}
-</div>
+</div> --}}
 
 <!--Reverso-->
 
-<div style="position: absolute; width: 204.10px; height: 323.15px; margin-left: 300px; margin-top: 0px;">
+{{-- <div style="position: absolute; width: 204.10px; height: 323.15px; margin-left: 300px; margin-top: 0px;">
     <img src="{{ $ruta_imagen_reverso }}" style="width: 204.10px; height: 323.15px;">
 </div>
 
@@ -73,4 +73,90 @@
             </td>
         </tr>
     </table>
-</div>
+</div> --}}
+
+
+<table style="width: 100%; border-collapse: collapse;">
+    <tr>
+        <!-- Frontal -->
+        <td style="width: 204.10px; height: 323.15px; padding: 0; vertical-align: top;">
+            <table style="width: 204.10px; height: 323.15px; border-collapse: collapse;">
+                <tr>
+                    <td style="padding: 0;">
+                        <img src="{{ $ruta_imagen }}" style="width: 204.10px; height: 323.15px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0; text-align: center; height: 120px;">
+                        <div style="margin-top: -320px;">
+                            <img src="{{ $ruta_imagen_estudiante }}" style="width:90px; height: 120px;">
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0; text-align: center;">
+                        <div style="margin-top: -230px; font-family: Arial; font-size: 10px; width: 140px; margin-left: auto; margin-right: auto;">
+                            {{ ucwords(strtolower($estudiante->nombre)) }}
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0; text-align: center;">
+                        <div style="margin-top: -207px; font-family: Arial; font-size: 10px; width: 140px; margin-left: auto; margin-right: auto;">
+                            C.I. {{ number_format($estudiante->cedula, 0, ',', '.') }}
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0; text-align: center;">
+                        <div style="margin-top: -195px; font-family: Arial; font-size: 10px; font-weight: bold; width: 140px; margin-left: auto; margin-right: auto;">
+                            {{ strtoupper($estudiante->carreras->nombre) }}
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </td>
+
+        <!-- Espacio entre carnet frontal y reverso -->
+        <td style="width: 96px;"></td>
+
+        <!-- Reverso -->
+        <td style="width: 204.10px; height: 323.15px; padding: 0; vertical-align: top;">
+            <table style="width: 204.10px; height: 323.15px; border-collapse: collapse;">
+                <tr>
+                    <td style="padding: 0;">
+                        <img src="{{ $ruta_imagen_reverso }}" style="width: 204.10px; height: 323.15px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0;">
+                        <div style="margin-top: -123px; text-align: center;">
+                            <img src="data:image/png;base64,{{ $qr_code }}" style="width:90px;">
+                            <img src="{{ $ruta_imagen_autoridad_sello }}" style="width:90px;">
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0;">
+                        <div style="margin-top: -50px;">
+                            <table style="width: 100%; border-collapse: collapse;">
+                                <tr>
+                                    <td style="font-family: Arial; font-size: 8px; width:90px; padding-left: 10px;">
+                                        Vence {{ date('d-m-Y', strtotime($carnet->fecha_vencimiento)) }}
+                                    </td>
+                                    <td style="font-family: Arial; font-size: 8px; width:90px; text-align: center;">
+                                        {{ $autoridad->nombre }}<br/>
+                                        {{ $autoridad->cargo }}<br/>
+                                        <span style="font-size: 6px;">
+                                            {{ $autoridad->resolucion }}
+                                        </span>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
